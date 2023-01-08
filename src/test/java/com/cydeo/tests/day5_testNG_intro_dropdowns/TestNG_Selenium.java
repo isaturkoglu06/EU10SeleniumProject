8aa7a14a818c1c125ae306fb9da5700e351583f2
isaturkoglu06 <112823751+isaturkoglu06@users.noreply.github.com>
2023-01-08 21:49:11 +0300

    committed day5

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
@@ -3,22 +3,35 @@ package com.cydeo.tests.day5_testNG_intro_dropdowns;
 import com.cydeo.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestNG_Selenium {
+    WebDriver driver;
 
-    @Test
-    public void selenium_test(){
+
+    @BeforeMethod
+    public void setUpMethod(){
         //Do browser driver setup
         //Open browser
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
 
         //Maximize the page
         driver.manage().window().maximize();
         //Implicit wait
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.close();
+    }
+    @Test
+    public void selenium_test(){
 
         //Get "https://google.com"
         driver.get("https://google.com");
