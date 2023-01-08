a15340322f47a4f10be8ef23f5e43fbda551b37a
isaturkoglu06 <112823751+isaturkoglu06@users.noreply.github.com>
2023-01-08 19:09:49 +0300

    committed day5

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
@@ -1,42 +1,48 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import com.cydeo.tests.utilities.WebDriverFactory;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.cydeo.tests.utilities.*;
+import org.openqa.selenium.*;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class T3_RadioButton_cont {
 
-    //1. Open Chrome browser
     public static void main(String[] args) {
 
-
-    WebDriver driver = WebDriverFactory.getDriver("chrome");
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    //2. Go to https//practice.cydeo.com/radio_buttons
+        //TC #2: Radiobutton handling
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //2. Go to https://practice.cydeo.com/radio_buttons
         driver.get("https://practice.cydeo.com/radio_buttons");
 
         //Locate name='sports' radio buttons and store them in a List of Web Element
-        List<WebElement> sportRadioButtons = driver.findElements(By.name("sport"));
+
+        clickAndVerifyRadioButton(driver, "sport", "hockey" );
+
+        clickAndVerifyRadioButton(driver, "sport", "football");
+
+        clickAndVerifyRadioButton(driver, "color", "yellow");
+
+    }
+
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameAttribute, String idValue){
+        List<WebElement> radioButtons = driver.findElements(By.name(nameAttribute));
 
         //Loop through the List of WebElement and select matching result "hockey"
-        for (WebElement each : sportRadioButtons) {
+        for (WebElement each : radioButtons) {
+            String eachId = each.getAttribute("id");
 
-            String eachId =  each.getAttribute("id");
-
-            System.out.println("eachId = " + eachId);
-
-            if(eachId.equals("hockey")){
+            if (eachId.equals(idValue)){
 
                 each.click();
-                System.out.println("Hockey is selected : " + each.isSelected());
+                System.out.println( eachId + " is selected : " + each.isSelected());
                 break;
             }
         }
+
     }
+
 }
